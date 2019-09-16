e50ef111d6e57963f39f9837ce168cd39f35b058
Rizwank <rizwan_k@outlook.com>
2019-09-16 12:54:51 +0200

    Variabel namen verandert

--- form.php
+++ form.php
@@ -1,105 +1,107 @@
 
-<!DOCTYPE HTML>  
+<!DOCTYPE HTML>
 <html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>  
-
+<body>
 <?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+	$beschrijfErr = $datumErr = $uiterlijkErr = $telnumErr = $woonplaatsErr = $addressErr = $voertuigErr = $ovragenErr = "";
+	$beschrijf = $datum = $uiterlijk = $telnum = $comment = $woonplaats = $address = $voertuig = $ovragen =	"";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
-    }
-  }
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["beschrijf"])) {
+	} else {
+		$beschrijf = test_input($_POST["beschrijf"]);
+		if (!preg_match("/^[a-zA-Z ]*$/",$beschrijf)) {
+			$beschrijfErr = "Only letters and white space allowed";
+	 	}
+	}
 
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
+	if (empty($_POST["datum"])) {
+			$datumErr = "datum is required";
+		} else {
+			$datum = test_input($_POST["datum"]);
+		if (!filter_var($datum, FILTER_VALIDATE_datum)) {
+		$datumErr = "Invalid datum format";
+		}
+	}
 
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
+	if (empty($_POST["uiterlijk"])) {
+		$uiterlijkErr = "uiterlijk is required";
+		} else {
+		$uiterlijk = test_input($_POST["uiterlijk"]);
+		}
+	}
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+	if (empty($_POST["telnum"])) {
+	} else {
+		$telnum = test_input($_POST["telnum"]);
+	}
+
+
+
+	if (empty($_POST["woonplaats"])) {
+	} else {
+		$woonplaats = test_input($_POST["woonplaats"]);
+	}
+
+	if (empty($_POST["address"])) {
+		$addressErr = "address is required";
+	} else {
+		$address = test_input($_POST["address"]);
+	}
+
+	if (empty($_POST["voertuig"])) {
+		} else {
+			$voertuig = test_input($_POST["voertuig"]);
+	}
+
+
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
 ?>
 
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Website: <input type="text" name="website" value="<?php echo $website;?>">
-  <span class="error"><?php echo $websiteErr;?></span>
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-  <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
+<h1>Politie-anoniem</h1>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
+	Beschrijf zo uitgebreid mogelijk de situatie waarover u wilt melden.<br>
+	<textarea beschrijf="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+	<span class="error"> <?php echo $beschrijfErr;?></span><br><br>
+
+	Op welke datum heeft dit woonplaatsgevonden of gaat dit gebeuren?<br>
+	<textarea beschrijf="comment" rows="1" cols="40"><?php echo $comment;?></textarea><br><br>
+
+	<h2>Weet u wie de dader is?</h2><br>
+
+	Kunt u meer vertellen over deze persoon of personen ?<br>
+	<input type="radio" beschrijf="uiterlijk" <?php if (isset($uiterlijk) && $uiterlijk=="ja") echo "checked";?> value="ja">ja<br>
+	<input type="radio" beschrijf="uiterlijk" <?php if (isset($uiterlijk) && $uiterlijk=="nee") echo "checked";?> value="nee">nee<br><br>
+
+	Weet u het telefoonnummer van de dader(s) ?<br>
+	<input type="radio" beschrijf="telnum" <?php if (isset($telnum) && $telnum=="ja") echo "checked";?> value="ja">ja<br>
+	<input type="radio" beschrijf="telnum" <?php if (isset($telnum) && $telnum=="nee") echo "checked";?> value="nee">nee<br><br>
+
+	<h2>Weet u waar dit speelt?</h2><br>
+
+	Woonplaats:<br> <textarea beschrijf="woonplaats" rows="1" cols="40"><?php echo $woonplaats;?></textarea><br><br>
+
+	Weet u het adres ?<br>
+	<input type="radio" beschrijf="address" <?php if (isset($address) && $address=="ja") echo "checked";?> value="ja">ja<br>
+	<input type="radio" beschrijf="address" <?php if (isset($address) && $address=="nee") echo "checked";?> value="nee">nee<br>
+	<input type="radio" beschrijf="address" <?php if (isset($address) && $address=="weet ik niet") echo "checked";?> value="weet ik niet">weet ik niet<br><br>
+
+	<h2>Voertuigen</h2>
+
+	Gebruikt de dader een voertuig ?<br>
+	<input type="radio" beschrijf="voertuig" <?php if (isset($voertuig) && $voertuig=="ja") echo "checked";?> value="ja">ja<br>
+	<input type="radio" beschrijf="voertuig" <?php if (isset($voertuig) && $voertuig=="nee") echo "checked";?> value="nee">nee<br><br>
+
+	Heeft u nog overige vragen?<br>
+	<textarea beschrijf="ovragen" rows="5" cols="40"><?php echo $ovragen;?></textarea><br><br>
+
+	<input  action="index.php" type="submit" name="Verstuur" value="Verstuur">
 </body>
 </html>
